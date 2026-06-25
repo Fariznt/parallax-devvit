7d590e89ec95fc195b8d2256ae3a924bf850ab83
Fariznt <fariz.imran98@gmail.com>
2026-06-25 12:30:28 -0400

    didnt have noEmit set to true in tsconfig so there were duplicate js/ts files from ts compilation. removed them and fixed tsconfig.json so it doesnt happen again

--- src/service/PolicyEngine/handlers/language.js
+++
@@ -1,6 +0,0 @@
-export function evalLanguage({ evalState, policyNode, negate, nodeAddress, evalNode, doEarlyExit, text, imageUrl, history,
-// models,
-// apiKeys,
- }) {
-    console.warn('Language nodes are not implemented');
-}
--- src/service/PolicyEngine/handlers/safety.js
+++
@@ -1,6 +0,0 @@
-export function evalSafety({ evalState, policyNode, negate, nodeAddress, evalNode, doEarlyExit, text, imageUrl, history,
-// models,
-// apiKeys,
- }) {
-    console.warn('Safety nodes are not implemented');
-}
